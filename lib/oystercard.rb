3e7b15d2b4b5c1ae60454f60e46bcd60e6142176
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-07-27 16:19:13 +0100

    saving_entry_station completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-attr_reader :balance
+attr_reader :balance, :entry_station
 
 MAX_LIMIT = 90
 MIN_LIMIT = 1
@@ -7,6 +7,7 @@ MIN_FARE = 0
 
   def initialize(balance=0)
     @balance = balance
+    @entry_station = entry_station
   end
 
   def top_up(amount)
@@ -19,17 +20,19 @@ MIN_FARE = 0
   end
 
   def in_journey?
-    @in_journey
+    !!entry_station
   end
 
-  def touch_in
+  def touch_in(station)
     fail "Insufficient funds to touch in" if underfare?
     @in_journey = true
+    @entry_station = station
   end
 
   def touch_out
     deduct (MIN_FARE)
     @in_journey = false
+    @entry_station = nil
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,9 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  let(:station){ double :station }
+
   it 'should have a balance of 0' do
     expect(subject.balance).to eq(0)
   end
@@ -27,26 +30,32 @@ describe Oystercard do
 
   it 'can touch in' do
     subject.top_up(1)
-    subject.touch_in
+    subject.touch_in(station)
     expect(subject).to be_in_journey
   end
 
   it 'can touch out' do
     subject.top_up(1)
-    subject.touch_in
+    subject.touch_in(station)
     subject.touch_out
     expect(subject).not_to be_in_journey
   end
 
   it 'will require £1 to touch in' do
-    expect{subject.touch_in}.to raise_error "Insufficient funds to touch in"
+    expect{subject.touch_in(station)}.to raise_error "Insufficient funds to touch in"
   end 
 
   it 'will deduct fare' do
     min_fare = Oystercard::MIN_FARE
     subject.top_up(1)
-    subject.touch_in
+    subject.touch_in(station)
     expect {subject.touch_out}.to change{subject.balance}.by(-min_fare)
   end 
+
+  it 'saves entry station' do
+    subject.top_up(1)
+    subject.touch_in(station)
+    expect(subject.entry_station).to eq station
+  end
 
 end
